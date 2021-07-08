ab0ebb7503a429d42a6182c41f82a902df118c2d
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-07-08 22:08:21 +0530

    Update app.js

--- node-express-jwt-auth/app.js
+++ node-express-jwt-auth/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://abantika-saha:abantika@@node-auth.ma1h9.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://abantika:25mongocfg21@node-auth.ma1h9.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
